b1e9628439e6975fd5fd154bf9e9c012950e6026
mi.nasonov <mi.nasonov@tensor.ru>
2026-05-09 10:59:40 +0300

    configure keymaps

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,26 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
+
+keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
+
+keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' })
+keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' })
+keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' })
+keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' })
+
+keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Go to right split' })
+keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Go to left split' })
+keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Go to bottom split' })
+keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Go to top split' })
+
+keymap.set('n', '<leader>bo', '<cmd>tabnew<CR>', { desc = 'Open new tab' })
+keymap.set('n', '<leader>bx', '<cmd>tabclose<CR>', { desc = 'Close current tab' })
+keymap.set('n', '<leader>bn', '<cmd>tabn<CR>', { desc = 'Go to next tab' })
+keymap.set('n', '<leader>bp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' })
+keymap.set('n', '<leader>bf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' })
+
+keymap.set('n', 'j', 'gj')
+keymap.set('n', 'k', 'gk')
